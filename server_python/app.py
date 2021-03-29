13142f9fcab31fed6bcc3af7f45f696b80c20a5a
Kevin Burk <percburk@gmail.com>
2021-03-28 22:10:08 -0500

    server GET and POST functional

--- server_python/app.py
+++ server_python/app.py
@@ -1,9 +1,41 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
+import os
+import pg8000.native
 
-# instantiate app
+# Instantiate app
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/test')
-def tester():
-    return "Hey, this is a test!"
+
+@app.route('/api/todos', methods=['GET', 'POST'])
+def todos():
+    conn = pg8000.dbapi.connect(
+        user=os.getenv('POSTGRES_USER'),
+        database="python_vue_todo"
+    )
+    cur = conn.cursor()
+
+    if request.method == 'GET':
+        cur.execute('SELECT * FROM "todos";')
+        rows = cur.fetchall()
+        keys = [k[0] for k in cursor.description]
+        results = [dict(zip(keys, row)) for row in rows]
+        conn.commit()
+        return jsonify(results)
+
+    elif request.method == 'POST':
+        new_task = request.get_json()
+        query = """
+            INSERT INTO "todos" ("task", "due_date", "priority")
+            VALUES (%s, %s, %s);
+        """
+        cur.execute(
+            query,
+            (new_task['task'], new_task['due_date'], new_task['priority'])
+        )
+        conn.commit()
+        return make_response('CREATED', 201)
+
+    else:
+        print('no routes found')
+        return 'no routes found'
