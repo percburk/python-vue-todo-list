969230914e9f7f8e3b4e549fc8bef15920b2b3f6
Kevin Burk <percburk@gmail.com>
2021-03-29 15:50:47 -0500

    formatting on sql queries, fixed id tuple  in delete route

--- server_python/app.py
+++ server_python/app.py
@@ -11,15 +11,15 @@ CORS(app)
 
 
 # GET and POST routes for todo table
-@app.route('/api/todos', methods=['GET', 'POST'])
+@app.route("/api/todos", methods=["GET", "POST"])
 def todos():
     conn = pg8000.dbapi.connect(
-        user=os.getenv('POSTGRES_USER'),
+        user=os.getenv("POSTGRES_USER"),
         database="python_vue_todo"
     )
     cur = conn.cursor()
 
-    if request.method == 'GET':
+    if request.method == "GET":
         cur.execute('SELECT * FROM "todos";')
         rows = cur.fetchall()
         keys = [k[0] for k in cur.description]
@@ -27,33 +27,33 @@ def todos():
         conn.commit()
         return results
 
-    elif request.method == 'POST':
+    elif request.method == "POST":
         new_task = request.get_json()
         query = """
             INSERT INTO "todos" ("task", "due_date", "priority")
             VALUES (%s, %s, %s);
         """
         cur.execute(query, (
-            new_task['task'],
-            new_task['due_date'],
-            new_task['priority']
+            new_task["task"],
+            new_task["due_date"],
+            new_task["priority"]
         ))
         conn.commit()
-        return make_response('CREATED', 201)
+        return make_response("CREATED", 201)
 
     else:
-        print('no routes found')
-        return 'no routes found'
+        print("no routes found")
+        return "no routes found"
 
 
 # DELETE route for todo table
-@app.route('/api/todos/<id>', methods=["DELETE"])
+@app.route("/api/todos/<id>", methods=["DELETE"])
 def delete_todo(id):
     conn = pg8000.dbapi.connect(
-        user=os.getenv('POSTGRES_USER'),
+        user=os.getenv("POSTGRES_USER"),
         database="python_vue_todo"
     )
     cur = conn.cursor()
-    cur.execute('DELETE FROM "todos" WHERE "id" = %s', (id))
+    cur.execute('DELETE FROM "todos" WHERE "id" = %s', (id,))
     conn.commit()
-    return make_response('DELETED', 204)
+    return make_response("DELETED", 204)
