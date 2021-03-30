8c0750ef2bf1327828bfde0830306b14e4c883f2
Kevin Burk <percburk@gmail.com>
2021-03-29 22:47:36 -0500

    proxy in env for axios routes

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,7 +1,9 @@
-import { createStore, createLogger, mapActions } from 'vuex';
+import { createStore, createLogger } from 'vuex';
 import axios from 'axios';
 // Models
 import { Task, NewTask } from '@/models/task.model';
+// Configure axios instance
+const axiosInstance = axios.create({ baseURL: process.env.VUE_APP_HTTP_PROXY });
 
 export default createStore({
   plugins: process.env.NODE_ENV === 'development' ? [createLogger()] : [],
@@ -15,26 +17,26 @@ export default createStore({
   },
   actions: {
     ADD_TASK({ dispatch }, task: NewTask) {
-      axios
-        .post('http://localhost:5000/api/todos', task)
+      axiosInstance
+        .post('/api/todos', task)
         .then(() => dispatch('FETCH_TASKS'))
         .catch((err) => console.log('Error in ADD_TASK', err));
     },
     TOGGLE_DONE_TASK({ dispatch }, id: number) {
-      axios
-        .put(`http://localhost:5000/api/todos/${id}`)
+      axiosInstance
+        .put(`/api/todos/${id}`)
         .then(() => dispatch('FETCH_TASKS'))
         .catch((err) => console.log('Error in TOGGLE_DONE_TASKS', err));
     },
     DELETE_TASK({ dispatch }, id: number) {
-      axios
-        .delete(`http://localhost:5000/api/todos/${id}`)
+      axiosInstance
+        .delete(`/api/todos/${id}`)
         .then(() => dispatch('FETCH_TASKS'))
         .catch((err) => console.log('Error in DELETE_TASK', err));
     },
     FETCH_TASKS({ commit }) {
-      axios
-        .get('http://localhost:5000/api/todos')
+      axiosInstance
+        .get('/api/todos')
         .then((response) => commit('SET_TASKS', response.data))
         .catch((err) => console.log('Error in FETCH_TASKS', err));
     },
